c60e4072f7546866c9efc467b6a898a84fd8e5a7
Knucklesx <gcfacchi@gmail.com>
2022-06-20 15:25:26 -0300

    FP

---
+++ src/context/foodProvider.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import foodContext from './foodContext';
+
+function foodProvider(props) {
+  
+  const fetchFood = async () => {
+    const foodCategoryURL = 'gttp'
+  }
+}
