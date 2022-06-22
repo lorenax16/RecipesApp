75120ca07c7a5c72a1f9cfa88db46f481293e1b8
Bruna Luchini <81397423+Luvlore@users.noreply.github.com>
2022-06-22 09:10:41 -0300

    feat: implementação de botões no footer

--- src/pages/components/DefaultTemplate/Footer/index.jsx
+++ src/pages/components/DefaultTemplate/Footer/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function Footer() {
+  const history = useHistory();
+
+  const toGoDrinks = () => history.push('/drinks');
+  const toGoExplore = () => history.push('/explore');
+  const toGoFoods = () => history.push('/foods');
+
   return (
-    <footer>
-      Footer
+    <footer data-testid="footer">
+      <button type="button" data-testid="drinks-bottom-btn" onClick={ toGoDrinks } />
+      <button type="button" data-testid="explore-bottom-btn" onClick={ toGoExplore } />
+      <button type="button" data-testid="food-bottom-btn" onClick={ toGoFoods } />
     </footer>
   );
 }
