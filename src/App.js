5d8886ddbe47f6e81452119059c4a72fb3ad1370
Bruna Luchini <brunaluchini22@gmail.com>
2022-06-26 17:18:23 -0300

    fix: requisitos 10 e 21

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import FoodsProvider from './context/foodContext';
-import { Login, FoodsPage, DrinksPage } from './pages';
+import { Login, FoodsPage, DrinksPage, RecipeInProgress } from './pages';
 import Profile from './pages/Profile';
 // import { NewHome } from './pages/Home/foodIndex';
 import { DefaultTemplate, RecipeDetails } from './pages/components';
@@ -16,20 +16,12 @@ function App() {
           <Route exact path="/foods/:id" component={ RecipeDetails } />
           <Route exact path="/drinks/:id" component={ RecipeDetails } />
 
-          <Route exact path="/foods/:id/in-progress" component={ RecipeDetails } />
-          <Route exact path="/drinks/:id/in-progress" component={ RecipeDetails } />
+          <Route exact path="/foods/:id/in-progress" component={ RecipeInProgress } />
+          <Route exact path="/drinks/:id/in-progress" component={ RecipeInProgress } />
 
           <DefaultTemplate>
             <Route exact path="/foods" component={ FoodsPage } />
             <Route exact path="/drinks" component={ DrinksPage } />
-
-            <Route exact path="/explore/foods" component={ RecipeDetails } />
-            <Route exact path="/explore/drinks" component={ RecipeDetails } />
-
-            <Route path="/explore/foods/nationalities" component={ RecipeDetails } />
-
-            <Route exact path="/explore/foods/ingredients" component={ RecipeDetails } />
-            <Route exact path="/explore/drinks/ingredients" component={ RecipeDetails } />
 
             <Route exact path="/profile" component={ Profile } />
 
