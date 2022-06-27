5c8d3635c57fe19ba2a47bc052aedc74d6260037
Knucklesx <gcfacchi@gmail.com>
2022-06-27 14:26:37 -0300

    inicio like button

--- src/pages/components/RecipeDetails/elements/index.jsx
+++ src/pages/components/RecipeDetails/elements/index.jsx
@@ -1,3 +1,4 @@
 export { default as RecCard } from './RecCard';
 export { default as Btn } from './Btn';
 export { default as IngredientsList } from './IngredientsList';
+export { default as LikeBtns } from './LikeBtn';
