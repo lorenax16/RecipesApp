bab8c552718427c5ece78fefc48c90aa384e3fca
lorena olivares <lorena.olivares.perez16@gmail.com>
2023-06-17 16:09:15 -0300

    modificações

---
+++ src/testes/01.loginPage.test.js
@@ -0,0 +1,8 @@
+// import { screen } from '@testing-library/react';
+// import React from 'react';
+
+describe('Testa tela de Login', () => {
+  it('testa i input de nome e email', () => {
+
+  });
+});
